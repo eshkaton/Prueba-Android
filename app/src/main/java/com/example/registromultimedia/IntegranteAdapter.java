c6cf21958a29e76e4136db3502590bdcc14fbb0c
zavalamarcelo110-web <zavalamarcelo110@gmail.com>
2026-09-22 02:07:45 -0300

    Refactor IntegranteAdapter to use List instead of ArrayList

--- app/src/main/java/com/example/registromultimedia/IntegranteAdapter.java
+++ app/src/main/java/com/example/registromultimedia/IntegranteAdapter.java
@@ -13,106 +13,106 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class IntegranteAdapter
-        extends RecyclerView.Adapter<IntegranteAdapter.ViewHolder> {
+                extends RecyclerView.Adapter<IntegranteAdapter.ViewHolder> {
 
-    private ArrayList<Integrante> listaIntegrantes;
+    private List<Integrante> listaIntegrantes;
 
-    public IntegranteAdapter(ArrayList<Integrante> listaIntegrantes) {
-        this.listaIntegrantes = listaIntegrantes;
+    public IntegranteAdapter(List<Integrante> listaIntegrantes) {
+                    this.listaIntegrantes = listaIntegrantes;
     }
 
 
     @NonNull
-    @Override
-    public ViewHolder onCreateViewHolder(
-            @NonNull ViewGroup parent,
-            int viewType) {
+                            @Override
+                            public ViewHolder onCreateViewHolder(
+                                                @NonNull ViewGroup parent,
+                                                int viewType) {
 
-        View vista = LayoutInflater
-                .from(parent.getContext())
-                .inflate(
-                        R.layout.item_integrante,
-                        parent,
-                        false
-                );
-        return new ViewHolder(vista);
-    }
-
-    @Override
-    public void onBindViewHolder(
-            @NonNull ViewHolder holder,
-            int position) {
-
-        Integrante integrante =
-                listaIntegrantes.get(position);
-        holder.txtNombre.setText(
-                integrante.getNombre()
-        );
-        holder.txtRol.setText(
-                integrante.getRol()
-        );
-        holder.ratingBar.setRating(
-                integrante.getValoracion()
-        );
-        holder.imgFoto.setImageResource(
-                integrante.getFotoResId()
-        );
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Context context = v.getContext();
-                Intent intent = new Intent(context, DetalleIntegranteActivity.class);
-
-
-                intent.putExtra("nombre_integrante", integrante.getNombre());
-                intent.putExtra("rol_integrante", integrante.getRol());
-                intent.putExtra("tecnologias_integrante", integrante.getTecnologias());
-                intent.putExtra("jornada_integrante", integrante.getJornada());
-                intent.putExtra("foto_integrante", integrante.getFotoResId());
-                intent.putExtra("valoracion_integrante", integrante.getValoracion());
-
-                context.startActivity(intent);
-            }
-        });
-    }
+                                View vista = LayoutInflater
+                                                        .from(parent.getContext())
+                                                        .inflate(
+                                                                                        R.layout.item_integrante,
+                                                                                        parent,
+                                                                                        false
+                                                                                );
+                                            return new ViewHolder(vista);
+                            }
 
     @Override
-    public int getItemCount() {
-        return listaIntegrantes.size();
-    }
+                            public void onBindViewHolder(
+                                                @NonNull ViewHolder holder,
+                                                int position) {
+
+                                Integrante integrante =
+                                                        listaIntegrantes.get(position);
+                                            holder.txtNombre.setText(
+                                                                    integrante.getNombre()
+                                                            );
+                                            holder.txtRol.setText(
+                                                                    integrante.getRol()
+                                                            );
+                                            holder.ratingBar.setRating(
+                                                                    integrante.getValoracion()
+                                                            );
+                                            holder.imgFoto.setImageResource(
+                                                                    integrante.getFotoResId()
+                                                            );
+                                            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                                                                @Override
+                                                                public void onClick(View v) {
+                                                                                        Context context = v.getContext();
+                                                                                        Intent intent = new Intent(context, DetalleIntegranteActivity.class);
+
+
+                                                                    intent.putExtra("nombre_integrante", integrante.getNombre());
+                                                                                        intent.putExtra("rol_integrante", integrante.getRol());
+                                                                                        intent.putExtra("tecnologias_integrante", integrante.getTecnologias());
+                                                                                        intent.putExtra("jornada_integrante", integrante.getJornada());
+                                                                                        intent.putExtra("foto_integrante", integrante.getFotoResId());
+                                                                                        intent.putExtra("valoracion_integrante", integrante.getValoracion());
+
+                                                                    context.startActivity(intent);
+                                                                }
+                                            });
+                            }
+
+    @Override
+                            public int getItemCount() {
+                                            return listaIntegrantes.size();
+                            }
 
 
 
     public static class ViewHolder
-            extends RecyclerView.ViewHolder {
-        TextView txtNombre;
-        TextView txtRol;
-        RatingBar ratingBar;
-        ImageView imgFoto;
+                        extends RecyclerView.ViewHolder {
+                                        TextView txtNombre;
+                                        TextView txtRol;
+                                        RatingBar ratingBar;
+                                        ImageView imgFoto;
 
-        public ViewHolder(
-                @NonNull View itemView) {
-            super(itemView);
+                                public ViewHolder(
+                                                        @NonNull View itemView) {
+                                                    super(itemView);
 
-            txtNombre =
-                    itemView.findViewById(
-                            R.id.tvNombre
-                    );
-            txtRol =
-                    itemView.findViewById(
-                            R.id.tvRol
-                    );
-            ratingBar =
-                    itemView.findViewById(
-                            R.id.ratingBar
-                    );
-            imgFoto =
-                    itemView.findViewById(
-                            R.id.imgFoto
-                    );
-        }
-    }
-}
+                                            txtNombre =
+                                                                        itemView.findViewById(
+                                                                                                            R.id.tvNombre
+                                                                                                    );
+                                                    txtRol =
+                                                                                itemView.findViewById(
+                                                                                                                    R.id.tvRol
+                                                                                                            );
+                                                    ratingBar =
+                                                                                itemView.findViewById(
+                                                                                                                    R.id.ratingBar
+                                                                                                            );
+                                                    imgFoto =
+                                                                                itemView.findViewById(
+                                                                                                                    R.id.imgFoto
+                                                                                                            );
+                                }
+                        }
+                }
